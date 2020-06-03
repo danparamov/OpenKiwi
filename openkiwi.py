228333069e4c402bc5d68caff1a87e6104158bda
danparamov <daniel.paramo.v@gmail.com>
2020-06-02 21:44:12 -0500

    Update openkiwi.py

--- openkiwi.py
+++ openkiwi.py
@@ -1,4 +1,4 @@
-import src.openkiwi.kiwi
-from src.openkiwi.kiwi.lib.train import *
+import kiwi
+from kiwi.lib.train import *
 
-predictor = train_from_file('./BERT_exploration/train_estimator.yaml')
+predictor = train_from_file('./experiments/train_estimator.yaml')
