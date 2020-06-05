b03172faa9cd2167b1c9cdba2b07995b51d925c6
danparamov <daniel.paramo.v@gmail.com>
2020-06-04 23:01:45 -0500

    Create train_predictor.py

---
+++ train_predictor.py
@@ -0,0 +1,37 @@
+import utils
+import yaml
+from ipywidgets import interact, fixed, Textarea
+from functools import partial
+import tarfile
+import pandas as pd
+import kiwi
+import os
+
+# Create the folder data/training and runs/predictor
+path = "./data/training"
+path2 = "./runs/predictor"
+os.makedirs(path)
+os.makedirs(path2)
+
+# Download Data
+OK_url = 'https://www.quest.dcs.shef.ac.uk/wmt20_files_qe/training_en-de.tar.gz'
+utils.download_kiwi(OK_url, "./data/training")
+
+# Extract Data
+my_tar = tarfile.open('./data/training/training_en-de.tar.gz')
+my_tar.extractall('./data/training') # specify which folder to extract to
+my_tar.close()
+
+# Reduction of Data
+# English
+tinytrainen = pd.read_csv('./data/training/train.ende.en',chunksize=10000, sep='None, /n', engine='python')
+readme = tinytrainen.get_chunk(10000)
+readme.to_csv(r'./data/training/tinytrainen', index=False, header=False)
+# German
+tinytrainde = pd.read_csv('./data/training/train.ende.de',chunksize=10000, sep='None, /n', engine='python')
+reader = tinytrainde.get_chunk(10000)
+reader.to_csv(r'./data/training/tinytrainde', index=False, header=False)
+
+#Run train_predictor.yaml file
+predictor_config = './experiments/train_predictor.yaml'
+kiwi.train(predictor_config)
