01adaf9303d86ff1782b3209cd30831363a92db1
danparamov <daniel.paramo.v@gmail.com>
2020-06-05 11:37:12 -0500

    Create download_trainingdata.py

---
+++ download_trainingdata.py
@@ -0,0 +1,13 @@
+import utils
+from ipywidgets import interact, fixed, Textarea
+from functools import partial
+import tarfile
+
+# Download Data
+OK_url = 'https://www.quest.dcs.shef.ac.uk/wmt20_files_qe/en-de.openkiwi-predictor.tar.gz'
+utils.download_kiwi(OK_url, "./data/traindev")
+
+# Extract Data
+my_tar = tarfile.open('./data/traindev/en-de.openwiki-predictor.tar.gz')
+my_tar.extractall('./data/traindev') # specify which folder to extract to
+my_tar.close()
