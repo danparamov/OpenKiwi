b9fa910bdfc49dcdd04ee16ccbfb86430bb44f7b
danparamov <daniel.paramo.v@gmail.com>
2020-06-05 11:35:51 -0500

    Update clean_dev.py

--- clean_dev.py
+++ clean_dev.py
@@ -1,12 +1,12 @@
 #development file
 
-file = open('./OpenKiwi/data/traindev/dev.ende.df.short.tsv')
+file = open('./data/traindev/en-de/dev.ende.df.short.tsv')
 data = file.readlines()[1:]
 file.close()
 
-de = open('./OpenKiwi/data/traindev/wmt20_dev.de', 'w')
-en = open('./OpenKiwi/data/traindev/wmt20_dev.en', 'w')
-hter = open('./OpenKiwi/data/traindev/wmt20_dev.hter_avg', 'w')
+de = open('./data/traindev/en-de/wmt20_dev.de', 'w')
+en = open('./data/traindev/en-de/wmt20_dev.en', 'w')
+hter = open('./data/traindev/en-de/wmt20_dev.hter_avg', 'w')
 for d in data:
 	d = d.split('\t')
 	print(d)
