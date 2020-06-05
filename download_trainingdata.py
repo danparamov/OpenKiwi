769d9fc7fc6213df76a5e44ead7eb4dfdd216c55
danparamov <daniel.paramo.v@gmail.com>
2020-06-05 13:06:56 -0500

    Update download_trainingdata.py

--- download_trainingdata.py
+++ download_trainingdata.py
@@ -8,6 +8,6 @@ OK_url = 'https://www.quest.dcs.shef.ac.uk/wmt20_files_qe/en-de.openkiwi-predict
 utils.download_kiwi(OK_url, "./data/traindev")
 
 # Extract Data
-my_tar = tarfile.open('./data/traindev/en-de.openwiki-predictor.tar.gz')
+my_tar = tarfile.open('./data/traindev/en-de.openkiwi-predictor.tar.gz')
 my_tar.extractall('./data/traindev') # specify which folder to extract to
 my_tar.close()
