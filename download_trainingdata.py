7868e989c54158f8f6c7dbf35214c32168632c82
danparamov <daniel.paramo.v@gmail.com>
2020-06-10 13:38:48 -0500

    Update download_trainingdata.py

--- download_trainingdata.py
+++ download_trainingdata.py
@@ -4,10 +4,10 @@ from functools import partial
 import tarfile
 
 # Download Data
-OK_url = 'https://www.quest.dcs.shef.ac.uk/wmt20_files_qe/en-de.openkiwi-predictor.tar.gz'
+OK_url = 'https://github.com/facebookresearch/mlqe/blob/master/data/en-de.tar.gz?raw=true'
 utils.download_kiwi(OK_url, "./data/traindev")
 
 # Extract Data
-my_tar = tarfile.open('./data/traindev/en-de.openkiwi-predictor.tar.gz')
+my_tar = tarfile.open('./data/traindev/en-de.tar.gz')
 my_tar.extractall('./data/traindev') # specify which folder to extract to
 my_tar.close()
